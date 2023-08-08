60a46bfec7a22d87c63e451f949f7db8adba6e7a
FabiRomano <99614961+FabiRomano@users.noreply.github.com>
2023-08-08 11:23:29 -0300

    task2 terminada

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,11 +1,15 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -15,11 +19,33 @@ public class HomebankingApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initData(ClientRepository clientRepository){
+	public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository){
 		return (args) ->{
-			Client client = new Client("123456", "juana", "perez", "hbcs@gmail");
-			clientRepository.save(client);
+			Client client1 = new Client("123456", "Melba", "Morel", "melba@mindhub.com");
+			clientRepository.save(client1);
+
+			Client client2 = new Client("98745612", "Guillermo", "Guevara", "guiGuevara@gmail");
+			clientRepository.save(client2);
+
+	//creo una cuenta
+			Account account1 = new Account( client1, "VIN001",LocalDate.now(), 5000);
+			accountRepository.save(account1);
+
+	//agrego otra cuenta con el metodo add
+			Account account2 = new Account(client1, "VIN002",LocalDate.now().plusDays(1), 7000);
+			client1.addAccount(account2);
+			accountRepository.save(account2);
+
+
+			Account account3 = new Account(client2, "VIN003",LocalDate.now().plusDays(1), 9000);
+			client2.addAccount(account3);
+			accountRepository.save(account3);
+
+
 		};
+
+
+
 	}
 
 }
--- src/main/java/com/mindhub/homebanking/models/Client.java
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -2,10 +2,9 @@ package com.mindhub.homebanking.models;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 
@@ -14,10 +13,15 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private long id;
+    @OneToMany(mappedBy="client", fetch=FetchType.EAGER)
+    private Set<Account> accounts = new HashSet<>();
+
     private String  dni;
     private String firstName;
     private String lastName;
     private String email;
+
+
 
     public Client() {
     }
@@ -28,6 +32,25 @@ public class Client {
         this.lastName = lastName;
         this.email = email;
     }
+
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+
+
+
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount(Account account){
+        account.setClient(this);
+        accounts.add(account);
+    }
+
 
     public long getId() {
         return id;
@@ -64,4 +87,7 @@ public class Client {
     public void setEmail(String email) {
         this.email = email;
     }
+
+
 }
+
